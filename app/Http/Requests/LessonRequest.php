fa346ac457253a2172f28e057a0ed335a692d595
sqwizXhar <sonagalkina127@gmail.com>
2024-12-08 23:21:17 +0300

    new request for lessons

---
+++ app/Http/Requests/LessonRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LessonRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'day_of_week' => 'in:true,false',
+            'date' => 'required_if:day_of_week,false|date|exists:dates,date',
+        ];
+    }
+}
