16af99d82d5f1344eb48e463860cdee1cbb1f1ae
sqwizXhar <sonagalkina127@gmail.com>
2024-12-08 23:27:12 +0300

    update realationship

--- app/Http/Resources/LessonResource.php
+++ app/Http/Resources/LessonResource.php
@@ -28,13 +28,13 @@ class LessonResource extends BaseResource
     {
         return array_merge(parent::toArray($request),
             [
-                'date' => $this->dates->select('date'),
-                'semester' => $this->semester,
+                'date' => $this->dates->pluck('date')->first(),
+                'semester' => $this->semester->number,
                 'day_of_week' => $this->day_of_week,
                 'time' => Carbon::parse($this->time)->format('H:i'),
                 'number_of_lesson' => $this->number_of_lesson,
                 'cabinet' => $this->cabinet ? $this->cabinet->number : null,
-                'group' => $this->group ? $this->group->name : null,
+                'group' => $this->semester->group ? $this->semester->group->name : null,
                 'subject' => $this->subject ? $this->subject->name : null,
                 'teacher' => $this->teacher ? $this->teacher->full_name : null,
             ]
