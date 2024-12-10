c7e0c1aa094a05fd8f6ed6b3a1714e21aec344ba
sqwizXhar <sonagalkina127@gmail.com>
2024-12-11 02:05:50 +0300

    Added BaseFormRequest

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseFormRequest extends FormRequest
+{
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], 422));
+    }
+}
