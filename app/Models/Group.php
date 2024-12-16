dacf3c18ce53d04d93f794b90dcd16e4ba7de3aa
sqwizXhar <sonagalkina127@gmail.com>
2024-12-16 22:25:19 +0300

    Added use of HasFactory in models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
     ];
