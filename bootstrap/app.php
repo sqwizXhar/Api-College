3d943723a34cf00ea1cda8cc3ed4f96821e2431e
sqwizXhar <sonagalkina127@gmail.com>
2024-12-19 03:57:51 +0300

    Configured routing and exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,21 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (Throwable $e, $request) {
+            if ($request->is('api/*')) {
+                if ($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+                    return response()->json([
+                        'error' => 'Такого URL не существует'
+                    ], 404);
+                }
+
+                if ($e instanceof AuthenticationException) {
+                    return response()->json([
+                        'error' => 'Неавторизован'
+                    ], 401);
+                }
+            }
+
+            return response()->json($e);
+        });
     })->create();
