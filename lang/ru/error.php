b9d1fc9e3c82fea1a9a901cd0f32d98356832b6a
sqwizXhar <sonagalkina127@gmail.com>
2024-12-19 04:00:13 +0300

    Added error localization files for ru and en

---
+++ lang/ru/error.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'invalid_role' => 'Недопустимая роль',
+    'role_cannot_have_groups' => 'Эта роль не может иметь группы',
+    'lesson_cannot_be_deleted' => 'Урок не может быть удален',
+];
