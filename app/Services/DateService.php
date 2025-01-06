ad646396e49b5948d2bd23c77f87117f8ed5e501
sqwizXhar <sonagalkina127@gmail.com>
2025-01-06 20:36:45 +0300

    Added getDatesSubject in DateService

--- app/Services/DateService.php
+++ app/Services/DateService.php
@@ -3,9 +3,16 @@
 namespace App\Services;
 
 use App\Models\Date;
+use App\Models\Grade;
+use Carbon\Carbon;
 
-class DateService
+class DateService extends BaseService
 {
+    public function __construct(Grade $grade)
+    {
+        parent::__construct($grade);
+    }
+
     public function getDates(array $validated)
     {
         $date = $validated['dates'];
@@ -22,5 +29,22 @@ class DateService
         }
 
         return $date->get();
+    }
+
+    public function getDatesSubject(array $validated)
+    {
+        $subject = $validated['subject'];
+
+        $today = Carbon::today();
+
+        $startDayOfMonth = $today->copy()->startOfMonth();
+
+        return Date::join('lessons', 'lessons.id', '=', 'dates.lesson_id')
+            ->join('subject_user', 'subject_user.id', '=', 'lessons.subject_user_id')
+            ->join('subjects', 'subjects.id', '=', 'subject_user.subject_id')
+            ->where('subjects.name', '=', $subject)
+            ->whereBetween('date', [$startDayOfMonth, $today])
+            ->select('dates.id', 'dates.date')
+            ->get();
     }
 }
